7365fb6548a21b06a4b54742108fe67cfe1d0602
fergo125 <ffmm.14@gmail.com>
2016-11-22 18:42:08 -0600

    Wsdl added and working now creating the user interface

---
+++ Client/Client_SOAP/src/client_soap/Client_SOAP.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package client_soap;
+
+/**
+ *
+ * @author Fergo
+ */
+public class Client_SOAP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
